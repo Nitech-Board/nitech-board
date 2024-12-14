a899d1300ce311860d734474fdbd3d8017fce32b
senntou <wataru_00117@icloud.com>
2024-12-14 23:03:23 +0900

    Headerのタイトルロゴ改修

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,16 @@
 import styles from "./Header.module.css";
 
+// aタグのリンクが横に長くなるのを防ぐために、divで囲んでいる
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1>Nitech Board</h1>
+      <div style={{ width: "fit-content" }}>
+        <a href="/">
+          <h1 className={`${styles.delaGothicOneRegular} ${styles.titleFont}`}>
+            Nitech Board
+          </h1>
+        </a>
+      </div>
     </header>
   );
 }
