7533b417f6f3cfc720b15387fce4a4dce6a9e1cc
Ry0ta3 <k.ryota326221@gmail.com>
2024-12-15 00:08:48 +0900

    メール認証（未完成）

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,19 @@
 "use client";
 
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
 // Firebaseの初期化を行うためfirebaseAppをインポート
 import { firebaseApp } from "../../lib/FirebaseConfig";
-
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import LoginForm from "../../components/login/LoginForm";
+//import { auth } from "@/lib/FirebaseConfig";
 
 export default function LoginPage() {
   // 状態管理
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>(""); // エラーメッセージの状態管理
 
   const router = useRouter();
 
@@ -23,15 +24,33 @@ export default function LoginPage() {
     //const auth = getAuth();
 
     // Firebaseで用意されているメールアドレスとパスワードでログインするための関数
-    signInWithEmailAndPassword(auth, email, password)
+    signInWithEmailAndPassword(auth, email, password) //authが悪いのかも？？
       .then((userCredential) => {
         const user = userCredential.user;
+
+        //メアドが確認済みかチェック
+        if (!user.emailVerified) {
+          setErrorMessage(
+            "メールアドレスが未確認です。確認メールをチェックしてください。"
+          );
+          return; //このreturnで不正アクセスをはじく
+        }
+
         // ログインができたかどうかをわかりやすくするためのアラート
         alert("ログインOK!");
         console.log(user);
         router.push("/search");
       })
       .catch((error) => {
+        // エラーメッセージのセット（代入）
+        if (
+          error.code === "auth/user-not-found" ||
+          error.code === "auth/wrong-password"
+        ) {
+          setErrorMessage("メールアドレスまたはパスワードが正しくありません。");
+        } else {
+          setErrorMessage("ログインに失敗しました。");
+        }
         console.log(error);
       });
   };
@@ -48,6 +67,8 @@ export default function LoginPage() {
         togglePasswordVisibility={() => setShowPassword(!showPassword)}
         onLogin={doLogin}
       />
+      {/* エラーのメッセージの表示 */}
+      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
     </div>
   );
 }
