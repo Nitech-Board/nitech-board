ffb33f1a2285ecee7f0fa194a4adeb2389aa2ccd
SoyMilk10-2 <hayato_ka@icloud.com>
2024-12-20 13:35:04 +0900

    検索ページの機能とデザインを作成した。

---
+++ src/components/Search/SearchForm.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import React, { useState } from "react";
+import styles from "./SearchForm.module.css";
+
+interface SearchFormProps {
+  onSearch: (query: { courseNumber: string; name: string }) => void;
+}
+
+export const SearchForm: React.FC<SearchFormProps> = ({ onSearch }) => {
+  const [courseNumber, setCourseNumber] = useState<string>("");
+  const [name, setName] = useState<string>("");
+
+  const handleCourseNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCourseNumber(e.target.value);
+  };
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handleSearchClick = () => {
+    onSearch({ courseNumber, name });
+  };
+
+  return (
+    <div className={styles.body}>
+      <p>時間割番号</p>
+      <p>
+        <input
+          type="text"
+          value={courseNumber}
+          onChange={handleCourseNumberChange}
+        />
+      </p>
+      <p>科目名</p>
+      <p>
+        <input type="text" value={name} onChange={handleNameChange} />
+      </p>
+      <p>
+        <button type="button" onClick={handleSearchClick}>
+          検索
+        </button>
+      </p>
+    </div>
+  );
+};
